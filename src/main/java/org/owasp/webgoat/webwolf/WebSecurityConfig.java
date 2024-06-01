ab10eef4a467adc8ceca3f1670cf6a77cca0a6c3
iN3ast <l.martinezb.2021@alumnos.urjc.es>
2024-06-01 15:11:24 +0200

    Update pom.xml to include spring-security-crypto dependency and its version

--- src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
@@ -36,7 +36,7 @@ import org.springframework.security.config.annotation.web.configurers.Expression
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.BCryptPasswordEncoder;
 
 /** Security configuration for WebGoat. */
 @Configuration
